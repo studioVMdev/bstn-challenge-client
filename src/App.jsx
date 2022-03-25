7b7be4beacbeac5c0d488be2df363dce68dba4c1
Alif <alifr22@yahoo.co.uk>
2022-03-25 09:50:59 +0000

    going to develop to pull

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,28 @@ import "./App.scss";
 // import PageSearch from "./pages/PageSearch";
 
 import { Route, Switch, Redirect } from "react-router-dom";
+import ConfirmationModal from "./components/ConfirmationModal/ConfirmationModal";
 
 const App = () => {
-	return (
-		<div className="App">
-			<Switch>
-				<Redirect from="/" to="/search" exact />
-
-				<Route
-					path="/calculate"
-					exact
-					// render={(routerProps) => <PageSearch {...routerProps} />}
-				/>
-
-				<Route
-					path="/search"
-					exact
-					// render={(routerProps) => <PageSearch {...routerProps} />}
-				/>
-			</Switch>
-		</div>
-	);
+  return (
+    <div className="App">
+      asd
+      {/* <ConfirmationModal></ConfirmationModal> */}
+      <Switch>
+        <Redirect from="/" to="/search" exact />
+        <Route
+          path="/calculate"
+          exact
+          // render={(routerProps) => <PageSearch {...routerProps} />}
+        />
+        <Route
+          path="/search"
+          exact
+          // render={(routerProps) => <PageSearch {...routerProps} />}
+        />
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/ConfirmationModal/ConfirmationModal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ConfirmationModal() {
+  return (
+    <div className="modal">
+      <div className="modal__exit-button"></div>
+      <div className="modal__container">
+        <div className="modal__globe">asdasf</div>
+        <div className="modal__text"></div>
+      </div>
+    </div>
+  );
+}
